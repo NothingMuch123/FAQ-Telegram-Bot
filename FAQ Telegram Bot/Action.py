f5e04878adec2aa386674dfa96d26f12da69667e
Potato <curious_boii@hotmail.com>
2021-08-07 21:30:26 +0800

    Added test function for media group

--- FAQ Telegram Bot/Action.py
+++ FAQ Telegram Bot/Action.py
@@ -1,11 +1,14 @@
+from typing import Tuple
+from telebot.types import InlineKeyboardMarkup
+
 class Action:
     def __init__(self, name : str) -> None:
         self.Name = name
 
 
     # Render the action for display
-    def Display(self) -> str:
-        return ""
+    def Display(self) -> Tuple[str, InlineKeyboardMarkup]:
+        return "", None
 
 
     # Render the feedback when selected
--- FAQ Telegram Bot/Bot.py
+++ FAQ Telegram Bot/Bot.py
@@ -3,7 +3,8 @@ from telebot import TeleBot, types
 
 
 # Load credentials
-credentials = json.load(open("FAQ Telegram Bot/Data/Credentials.json", "r"))
+DataDirectory = "FAQ Telegram Bot/Data/"
+credentials = json.load(open(DataDirectory + "Credentials.json", "r"))
 
 # Create bot
 bot = bot = TeleBot(credentials["token"])
@@ -13,8 +14,19 @@ def SendMessage(user, message):
     bot.send_message(user, message)
 
 
-@bot.message_handler(commands=['start'])
+def SendMediaGroup(user):
+    # Send image
+    img1 = open(DataDirectory + "Chigua.png", "rb")
+    img2 = open(DataDirectory + "clowntoclown.jpg", "rb")
+    imgs = [types.InputMediaPhoto(img1, "Test Caption 1"), types.InputMediaPhoto(img2, "Test Caption 2")]
+    bot.send_media_group(user, imgs)
+    img1.close()
+    img2.close()
+
+
+@bot.message_handler(commands=["start"])
 def Start_Command(m):
+    # Send main message
     SendMessage(m.chat.id, "Test message")
 
 
